7b74c325978dd92fe266dea1bca842f207462704
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-30 08:29:20 -0500

    Updated from Brython Server: 1/30/2018 8:29:19 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,11 @@
+#Maia Reynolds
+#1/30/18
+#movie.py - tells user what movie they can watch
 
+age=int(input("Enter your age: "))
+if age<13:
+    print("You can watch PG movies")
+elif age<17:
+    print("You can watch PG-13 movies")
+else:
+    print("You can watch R movies")
