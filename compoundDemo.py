3328ee121234c2f8111a5ff4e68ba7fba4567a88
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-30 08:17:04 -0500

    Updated from Brython Server: 1/30/2018 8:17:03 AM

--- compoundDemo.py
+++ compoundDemo.py
@@ -5,5 +5,9 @@
 num=int(input("Enter a number: "))
 if num>0 and num%7==0:
     print(num,"is positive and divisible by 7")
-elif num>0 and num%7!=0:
+elif num>0:
     print(num,"is positive and not divisible by 7")
+elif num<0 and num%7==0:
+    print(num,"is negative and divisible by 7")
+elif num<0:
+    print(num,"is negative and not divisible by 7")
