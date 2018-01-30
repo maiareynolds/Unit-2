d1833b7bb84eb1e44e7d1da7f0f8d7ba051b796c
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-30 08:24:27 -0500

    Updated from Brython Server: 1/30/2018 8:24:26 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,15 @@
+#Maia Reynolds
+#1/30/18
+#gradeCalculator.py - percentage to letter grade
 
+grade=int(input("Enter your grade as a percent: "))
+if grade>=90:
+    print("You earned a A")
+elif grade>=80:
+    print("You earned a B")
+elif grade>=70:
+    print("You earned a C")
+elif grade>=60:
+    print("You earned a D")
+else:
+    print("You earned a NC")
