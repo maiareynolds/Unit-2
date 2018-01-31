41160b36fa041962e9cb996bdc48e77ef9c4000e
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-31 08:29:50 -0500

    Updated from Brython Server: 1/31/2018 8:29:49 AM

--- unitConverter.py
+++ unitConverter.py
@@ -12,8 +12,12 @@ if number==1:
     print(km,"kilometers is",round(0.621371*km,5),"miles")
 elif number==2:
     kg=int(input("Enter weight in kilograms: "))
-    print(kg,"kilograms is",round(kg*
+    print(kg,"kilograms is",round(kg*2.20462,5),"pounds")
 elif number==3:
     l=int(input("Enter volume in liters: "))
+    print(l,"liters is",round(l*0.264172,5),"gallons")
 elif number==4:
-    c=int(input("Enter degrees in celcius: "))
+    c=int(input("Enter degrees in Celcius: "))
+    print(c,"degrees Celcius is",round((c*1.8)+32,5),"degrees Fahrenheit")
+else:
+    print("Wrong Number, Try Again")
