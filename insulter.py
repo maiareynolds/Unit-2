b997de26643a3ec10c3a67069a67c752a68a0d29
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-02 08:39:15 -0500

    Updated from Brython Server: 2/2/2018 8:39:14 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,17 @@
+#Maia Reynolds
+#2/2/18
+#insulter.py - gives insult
 
+from random import randint
+name=input("Enter your name: ")
+number=randint(1,5)
+if number==1:
+    print(name,"is a stupid name")
+elif number==2:
+    print("You have horrible taste")
+elif number==3:
+    print("You smell disgusting")
+elif number==4:
+    print("You are such an idiot")
+else:
+    print("Your shirt is ugly")
