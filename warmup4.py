4da52999f81e1266cb530bded85bdbb240ab7a58
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-02 08:24:46 -0500

    Updated from Brython Server: 2/2/2018 8:24:45 AM

--- warmup4.py
+++ warmup4.py
@@ -1,1 +1,9 @@
+#Maia Reynolds
+#2/2/18
+#warmup4.py - ask user for number, print if divisible by 7 or has a 7, or print number
 
+number=int(input("Enter a number: "))
+if number%7==0 or "7" in str(number):
+    print("Buzz")
+else:
+    print(number)
