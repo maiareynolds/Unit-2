44a396a08a188acea0134bd4f604381d5af1fb2f
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-02 08:47:38 -0500

    Updated from Brython Server: 2/2/2018 8:47:38 AM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,6 @@
+#Maia Reynolds
+#2/2/18
+#adventure.py - if statements inside if statements to make adventure
 
+print("You are walking on a city sidewalk and reach a crosswalk. Do you cross the street or keep
+one
