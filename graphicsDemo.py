56d2714bd6aa53fd65554dba4b952cfad327e049
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-07 08:10:31 -0500

    Updated from Brython Server: 2/7/2018 8:10:30 AM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -1,1 +1,12 @@
+#Maia Reynolds
+#2/7/18
+#graphicsDemo.py - intro to ggame
 
+from ggame import *
+red=Color(0xFF0000,1) #this is the color red
+black=Color(0x000000,1) #this is the color black
+
+blackOutline=LineStyle(1,black)
+redRectangle=RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
+
+Sprite(redRectangle)
