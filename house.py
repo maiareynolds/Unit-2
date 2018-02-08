c7bc5d7e714afbac27eb12c92efc891033e39ab9
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-08 16:59:18 -0500

    Updated from Brython Server: 2/8/2018 4:59:17 PM

--- house.py
+++ house.py
@@ -5,15 +5,20 @@
 from ggame import *
 basecolor=Color(0x00B2EE,1)
 outlinecolor=Color(0x00688B,1)
+roofoutlinecolor=Color(0x00688B,1)
+roofcolor=Color(0x696969,1)
 baseoutline=LineStyle(1,outlinecolor)
-
+roofoutline=LineStyle(1,roofoutlinecolor)
 
 front=PolygonAsset([(400,200),(500,100),(600,200),(600,300),(400,300)],baseoutline,basecolor)
 side=PolygonAsset([(400,300),(300,250),(300,150),(400,200)],baseoutline,basecolor)
-roof
+roof=PolygonAsset([(500,100),(390,210),(290,160),(400,50)],roofoutline,roofcolor)
+
+500,100)(390,210)(290,160)(400,50)
 
 Sprite(front,(400,100))
 Sprite(side,(300,150))
+Sprite(roof,(290,50))
 App().run()
 
 blackOutline=LineStyle(1,black)
