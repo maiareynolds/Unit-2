60242ab422116266218a62403f847a70bd6e82c2
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-08 12:09:18 -0500

    Updated from Brython Server: 2/8/2018 12:09:17 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -16,11 +16,14 @@ blueCircle=CircleAsset(50,blackOutline,blue) #radius, outline, fill
 greenEllipse=EllipseAsset(100,50,blackOutline,green) #width, height, outline, fill
 blackLine=LineAsset(500,100,blackOutline) #to x over, to y over, lineStyle
 purpleTriangle=PolygonAsset([(0,100),(50,200),(100,100)],blackOutline,purple) #endpoints, outline, fill
+text=TextAsset("Maia", fill=blue, style="bold 60pt Times") #What you want, other options
+
 
 Sprite(redRectangle)
 Sprite(blueCircle,(200,0)) #... ,(over x, over y)
 Sprite(greenEllipse,(300,0))
 Sprite(blackLine)
 Sprite(purpleTriangle)
+Sprite(text,(600,0))
 
 App().run()
