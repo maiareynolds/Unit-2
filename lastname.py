280e807cb9451fe9a671f830ddcd914fa60ba6c6
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-12 06:47:07 -0500

    Updated from Brython Server: 2/12/2018 6:47:06 AM

--- lastname.py
+++ lastname.py
@@ -1,1 +1,9 @@
+#Maia Reynolds
+#2/12/18
+#lastname.py - tells what half of the alphoabet is in
 
+name=input("Enter your last name: ")
+if name<"N":
+    print(name,"is in the first half of the alphabet")
+else:
+    print(name,"is in the second half of the alphabet")
